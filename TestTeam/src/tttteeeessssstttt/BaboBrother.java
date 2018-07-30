b982048d777217b31f34cc888cc5bb113e542208
kosta <kosta@kosta-HP>
2018-07-30 14:39:52 +0900

    brsnche

--- TestTeam/src/tttteeeessssstttt/BaboBrother.java
+++ TestTeam/src/tttteeeessssstttt/BaboBrother.java
@@ -1,5 +1,11 @@
 package tttteeeessssstttt;
 
 public class BaboBrother {
-	int i = 1;
+	public static void main(String[] args) {
+		int i = 1;
+		String name = "KSY";
+		String zz= "Branches";
+		System.out.println(name+zz+"여요");
+	}
+	
 }
